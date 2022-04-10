dc69467dff1b0ce5b8776baffec301edc221775c
Io <wmt.tkn.121@gmail.com>
2022-04-10 23:50:19 +0900

    test pyaudio

--- src/hear_wav.py
+++ src/hear_wav.py
@@ -4,3 +4,15 @@ import numpy as np
 
 pa.datasets.CMUArcticCorpus(basedir='./CMU_ARCTIC',
                             download=True, speaker=['aew', 'axb'])
+
+sample_wave_file = './CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav'
+
+wav = wave.open(sample_wave_file)
+
+print(wav.getframerate())
+
+data = wav.readframes(wave.getnframes())
+
+data = np.frombuffer(data, dtype=np.int16)
+
+wav.close()
