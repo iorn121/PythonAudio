e2449280f8b315e8d0e98c2385729d723cf931bd
Io <wmt.tkn.121@gmail.com>
2022-04-11 01:53:26 +0900

    edit hear_wav

--- src/hear_wav.py
+++ src/hear_wav.py
@@ -1,17 +1,17 @@
 import wave as wave
-import pyroomacoustics as pa
 import numpy as np
 
-pa.datasets.CMUArcticCorpus(basedir='./CMU_ARCTIC',
-                            download=True, speaker=['aew', 'axb'])
 
-sample_wave_file = './CMU_ARCTIC/cmu_us_aew_arctic/wav/arctic_a0001.wav'
+sample_wave_file = './WAV/file_example_WAV_1MG.wav'
 
 wav = wave.open(sample_wave_file)
 
-print(wav.getframerate())
+print('サンプリング周波数[Hz]', wav.getframerate())
+print('サンプルサイズ[Byte]', wav.getsampwidth())
+print('サンプル数', wav.getnframes())
+print('チャンネル数', wav.getnchannels())
 
-data = wav.readframes(wave.getnframes())
+data = wav.readframes(wav.getnframes())
 
 data = np.frombuffer(data, dtype=np.int16)
 
