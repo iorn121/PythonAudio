af0ce39b86001bb14a6236ab54a705431f57ef5f
Io <wmt.tkn.121@gmail.com>
2022-04-12 23:21:59 +0900

    make wave graph

--- src/show_wav.py
+++ src/show_wav.py
@@ -1,5 +1,6 @@
 import wave as wave
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 sample_wave_file = './WAV/file_example_WAV_1MG.wav'
@@ -14,5 +15,15 @@ print('チャンネル数', wav.getnchannels())
 data = wav.readframes(wav.getnframes())
 
 data = np.frombuffer(data, dtype=np.int16)
+
+
+data = data/np.iinfo(np.int16).max
+x = np.array(range(2*wav.getnframes()))/wav.getframerate()
+plt.figure(figsize=(10, 4))
+plt.xlabel('Time[sec]')
+plt.ylabel('Value[-1,1]')
+plt.plot(x, data)
+plt.savefig('./IMG/wave_form.png')
+plt.show()
 
 wav.close()
