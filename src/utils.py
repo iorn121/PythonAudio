420b7349cbcfaca2846a8e6eab4073a752231429
Io <wmt.tkn.121@gmail.com>
2022-05-12 22:23:17 +0900

    plus record function

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,16 @@ def play(data, frame_rate):
     sd.play(data, frame_rate)
     print("Play sound...")
     status = sd.wait()
+
+
+def record(secoond_length,frame_rate):
+    print("Record...")
+    data=sd.rec(int(secoond_length*frame_rate), frame_rate,channels=1)
+    sd.wait()
+    print("Complete")
+    return data
+
+frame_rate=16000
+data=record(3,frame_rate)
+
+play(data, frame_rate)
