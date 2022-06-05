981bbc4470c9f3972a2d28cd3e9a6e0c3697d8a3
Io <wmt.tkn.121@gmail.com>
2022-06-05 22:01:37 +0900

    create plt module

--- src/test.py
+++ src/test.py
@@ -1,4 +1,10 @@
+import wave as wave
+from utils import save_sound_wave,save_sound_info
 import numpy as np
+import matplotlib.pyplot as plt
 
-A = np.iinfo(np.int16)
-print(A)
+data=np.random.normal(scale=0.1,size=5000)
+data_scale_adjust=data*np.iinfo(np.int16).max
+print(np.iinfo(np.int16))
+plt.hist(data_scale_adjust, bins=100)
+plt.show()
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import wave as wave
 import sounddevice as sd
+import logging
+import os
 
 
-def play(data, frame_rate):
+def play_sound(data, frame_rate):
     sd.play(data, frame_rate)
     print("Play sound...")
     status = sd.wait()
 
 
-def record(secoond_length,frame_rate):
+def record_sound(second_length,frame_rate):
     print("Record...")
-    data=sd.rec(int(secoond_length*frame_rate), frame_rate,channels=1)
+    data=sd.rec(int(second_length*frame_rate), frame_rate,channels=1)
     sd.wait()
     print("Complete")
     return data
 
-frame_rate=16000
-data=record(3,frame_rate)
+def save_sound_info(data,name):
+    info=data.getparams()
+    f=open(name+"_info.txt",'w')
+    params=['nchannels', 'sampwidth', 'framerate', 'nframes', 'comptype', 'compname']
+    for p,i in zip(params,info):
+        print(p,i)
+        f.write(p+': '+str(i)+'\n')
+    f.close()
 
-play(data, frame_rate)
+def save_sound_wave(data,name):
+    buff=data.readframes(-1)
+    convert=np.frombuffer(buff,dtype=np.int16)
+    if data.getnchannels()==2:
+        convert_l=convert[::2]
+        convert_r=convert[1::2]
+        plt.subplot(211)
+        plt.plot(convert_l)
+        plt.subplot(212)
+        plt.plot(convert_r)
+    else:
+        plt.plot(convert)
+    plt.savefig(name+"_wave.png")
