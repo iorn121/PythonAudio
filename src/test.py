9573b22906d98072c0c1b586558175858fef552e
Io <wmt.tkn.121@gmail.com>
2022-06-06 23:26:56 +0900

    scipy

--- src/test.py
+++ src/test.py
@@ -1,10 +1,7 @@
 import wave as wave
 from utils import save_sound_wave,save_sound_info
 import numpy as np
+import scipy.signal as sp
 import matplotlib.pyplot as plt
 
-data=np.random.normal(scale=0.1,size=5000)
-data_scale_adjust=data*np.iinfo(np.int16).max
-print(np.iinfo(np.int16))
-plt.hist(data_scale_adjust, bins=100)
-plt.show()
+data=wave.open("./WAV/jihou_tadaima.wav","rb")
