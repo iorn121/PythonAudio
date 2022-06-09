edc3c9ab947d4b13a11f909fe10669314cf0c2c8
iorn <wmt.tkn.121@gmail.com>
2022-06-09 18:01:13 +0900

    test edit

--- src/test.py
+++ src/test.py
@@ -1,19 +1,18 @@
 from calendar import c
 import wave as wave
-from utils import save_sound_wave,save_sound_info,record_sound
+from utils import save_sound_wave, save_sound_info, record_sound, save_sound_frequency, play_sound
 import numpy as np
 import scipy.signal as sp
 import matplotlib.pyplot as plt
 
-name='test'
-record_sound(5,16000,name)
-data=wave.open("./WAV/test.wav","rb")
-buff=data.readframes(-1)
-convert=np.frombuffer(buff,dtype=np.int16)
-fig=plt.figure(figsize=(10,4))
-spectrum,freqs,t,im=plt.specgram(convert,NFFT=512,noverlap=512/16*15,Fs=data.getframerate(),cmap="gray")
-fig.colorbar(im).set_label('Intensity [dB]')
-plt.xlabel('Time [sec]')
-plt.ylabel('Frequency [Hz]')
-plt.savefig('./jihou_tadaima_spectrum.png')
-plt.show()
+name = 'test'
+frame_rate = 16000
+data = record_sound(5, frame_rate, name)
+
+f, t, stft_data = sp.stft(
+    data, fs=frame_rate, window='hann', nperseg=512, noverlap=256)
+stft_data[100:, :] = 0
+t, data_post = sp.istft(stft_data, fs=frame_rate,
+                        window='hann', nperseg=512, noverlap=256)
+save_sound_wave(data_post, name)
+play_sound(data_post, frame_rate)
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import wave as wave
 import sounddevice as sd
-import soundfile as sf
+# import soundfile as sf
 import logging
 import os
 
@@ -13,28 +13,44 @@ def play_sound(data, frame_rate):
     status = sd.wait()
 
 
-def record_sound(second_length,frame_rate,name):
+def record_sound(second_length, frame_rate, name):
     print("Record...")
-    data=sd.rec(int(second_length*frame_rate), frame_rate,channels=1)
+    data = sd.rec(int(second_length*frame_rate), frame_rate, channels=1)
     sd.wait()
     print("Complete")
-    sf.write('./WAV/'+name+'.wav',data,frame_rate)
+    # ノーマライズ。量子化ビット16bitで録音するので int16 の範囲で最大化する
+    data = data / data.max() * np.iinfo(np.int16).max
 
-def save_sound_info(data,name):
-    info=data.getparams()
-    f=open(name+"_info.txt",'w')
-    params=['nchannels', 'sampwidth', 'framerate', 'nframes', 'comptype', 'compname']
-    for p,i in zip(params,info):
-        print(p,i)
+    # float -> int
+    data = data.astype(np.int16)
+    # Macではsoundfileがエラー起こして動かせないっぽい
+    # sf.write('./WAV/'+name+'.wav', data, frame_rate)
+
+    with wave.open('./WAV/'+name+'.wav', mode='wb') as wb:
+        wb.setnchannels(1)  # モノラル
+        wb.setsampwidth(2)  # 16bit=2byte
+        wb.setframerate(frame_rate)
+        wb.writeframes(data.tobytes())  # バイト列に変換
+    return data
+
+
+def save_sound_info(data, name):
+    info = data.getparams()
+    f = open(name+"_info.txt", 'w')
+    params = ['nchannels', 'sampwidth', 'framerate',
+              'nframes', 'comptype', 'compname']
+    for p, i in zip(params, info):
+        print(p, i)
         f.write(p+': '+str(i)+'\n')
     f.close()
 
-def save_sound_wave(data,name):
-    buff=data.readframes(-1)
-    convert=np.frombuffer(buff,dtype=np.int16)
-    if data.getnchannels()==2:
-        convert_l=convert[::2]
-        convert_r=convert[1::2]
+
+def save_sound_wave(data, name):
+    buff = data.readframes(-1)
+    convert = np.frombuffer(buff, dtype=np.int16)
+    if data.getnchannels() == 2:
+        convert_l = convert[::2]
+        convert_r = convert[1::2]
         plt.subplot(211)
         plt.plot(convert_l)
         plt.subplot(212)
@@ -42,3 +58,16 @@ def save_sound_wave(data,name):
     else:
         plt.plot(convert)
     plt.savefig(name+"_wave.png")
+
+
+def save_sound_frequency(data, name):
+    buff = data.readframes(-1)
+    convert = np.frombuffer(buff, dtype=np.int16)
+    fig = plt.figure(figsize=(10, 4))
+    spectrum, freqs, t, im = plt.specgram(
+        convert, NFFT=512, noverlap=512/16*15, Fs=data.getframerate(), cmap="gray")
+    fig.colorbar(im).set_label('Intensity [dB]')
+    plt.xlabel('Time [sec]')
+    plt.ylabel('Frequency [Hz]')
+    plt.savefig('./'+name+'_spectrum.png')
+    plt.show()
