ad8175b198e6c110724aac4ae72dc421a6b7724b
iorn <wmt.tkn.121@gmail.com>
2022-06-12 11:21:43 +0900

    一旦数学の勉強

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 import wave as wave
-from utils import save_sound_wave, save_sound_info, record_sound, save_sound_frequency, play_sound
+from utils import save_sound_wave, save_sound_info, record_sound, save_sound_frequency, play_sound, convert_wave_to_np, save_wave_file
 import numpy as np
 import scipy.signal as sp
 import matplotlib.pyplot as plt
@@ -7,25 +7,11 @@ import matplotlib.pyplot as plt
 name = 'test'
 frame_rate = 16000
 # record_sound(5, frame_rate, name)
-data=wave.open('./WAV/test.wav')
-print(type(data))
-buff = data.readframes(-1)
-convert = np.frombuffer(buff, dtype=np.int16)
-f, t, stft_data = sp.stft(
-    x=convert,fs=frame_rate, window='hann', nperseg=512, noverlap=256)
-stft_data[100:, :] = 0
-t, data_post = sp.istft(stft_data, fs=frame_rate, window='hann', nperseg=512, noverlap=256)
-
-fig = plt.figure(figsize=(10, 4))
-spectrum, freqs, t, im = plt.specgram(
-    data_post, NFFT=512, noverlap=512/16*15, Fs=data.getframerate(), cmap="gray")
-fig.colorbar(im).set_label('Intensity [dB]')
-plt.xlabel('Time [sec]')
-plt.ylabel('Frequency [Hz]')
-plt.savefig('./'+name+'_post_spectrum.png')
-plt.show()
-with wave.open('./WAV/'+name+'_post.wav', mode='wb') as wb:
-    wb.setnchannels(1)  # モノラル
-    wb.setsampwidth(2)  # 16bit=2byte
-    wb.setframerate(frame_rate)
-    wb.writeframes(data_post.tobytes())  # バイト列に変換
+data_wav = wave.open('./WAV/test.wav')
+data_np = convert_wave_to_np(data_wav)
+f, t, stft_data = sp.stft(data_np, fs=frame_rate, window='hann',
+                          nperseg=512, noverlap=256)
+stft_data[:50, :] = 0
+t, data_post = sp.istft(stft_data, fs=frame_rate,
+                        window='hann', nperseg=512, noverlap=256)
+play_sound(data_np, frame_rate)
--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,8 @@ import logging
 import os
 
 
-def play_sound(data, frame_rate):
-    sd.play(data, frame_rate)
+def play_sound(data_np, frame_rate):
+    sd.play(data_np, frame_rate)
     print("Play sound...")
     status = sd.wait()
 
@@ -19,22 +19,25 @@ def record_sound(second_length, frame_rate, name):
     sd.wait()
     print("Complete")
     # ノーマライズ。量子化ビット16bitで録音するので int16 の範囲で最大化する
-    # data = data / data.max() * np.iinfo(np.int16).max
+    data = data / data.max() * np.iinfo(np.int16).max
 
     # float -> int
     data = data.astype(np.int16)
     # Macではsoundfileがエラー起こして動かせないっぽい
     # sf.write('./WAV/'+name+'.wav', data, frame_rate)
+    save_wave_file(data, frame_rate, name)
 
+
+def save_wave_file(data_np, frame_rate, name):
     with wave.open('./WAV/'+name+'.wav', mode='wb') as wb:
         wb.setnchannels(1)  # モノラル
         wb.setsampwidth(2)  # 16bit=2byte
         wb.setframerate(frame_rate)
-        wb.writeframes(data.tobytes())  # バイト列に変換
+        wb.writeframes(data_np.tobytes())  # バイト列に変換
 
 
-def save_sound_info(data, name):
-    info = data.getparams()
+def save_sound_info(data_wav, name):
+    info = data_wav.getparams()
     f = open(name+"_info.txt", 'w')
     params = ['nchannels', 'sampwidth', 'framerate',
               'nframes', 'comptype', 'compname']
@@ -44,27 +47,29 @@ def save_sound_info(data, name):
     f.close()
 
 
-def save_sound_wave(data, name):
-    buff = data.readframes(-1)
+def convert_wave_to_np(data_wav):
+    buff = data_wav.readframes(-1)
     convert = np.frombuffer(buff, dtype=np.int16)
-    if data.getnchannels() == 2:
-        convert_l = convert[::2]
-        convert_r = convert[1::2]
+    return convert
+
+
+def save_sound_wave(data_np, channel_n, name):
+    if channel_n == 2:
+        data_np_l = data_np[::2]
+        data_np_r = data_np[1::2]
         plt.subplot(211)
-        plt.plot(convert_l)
+        plt.plot(data_np_l)
         plt.subplot(212)
-        plt.plot(convert_r)
+        plt.plot(data_np_r)
     else:
-        plt.plot(convert)
+        plt.plot(data_np)
     plt.savefig(name+"_wave.png")
 
 
-def save_sound_frequency(data, name):
-    buff = data.readframes(-1)
-    convert = np.frombuffer(buff, dtype=np.int16)
+def save_sound_frequency(data_np, frame_rate, name):
     fig = plt.figure(figsize=(10, 4))
     spectrum, freqs, t, im = plt.specgram(
-        convert, NFFT=512, noverlap=512/16*15, Fs=data.getframerate(), cmap="gray")
+        data_np, NFFT=512, noverlap=512/16*15, Fs=frame_rate, cmap="gray")
     fig.colorbar(im).set_label('Intensity [dB]')
     plt.xlabel('Time [sec]')
     plt.ylabel('Frequency [Hz]')
